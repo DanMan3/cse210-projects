60567de27f082874b7479c50c8631613adecf8e4
DanMan3 <dhbennion@gmail.com>
2025-10-30 17:16:10 -0600

    W02 Learning Activity: Abstraction added.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Canonical";
+        job1._startYear = "2020";
+        job1._endYear = "2025";
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Web Developer";
+        job2._company = "Microsoft";
+        job2._startYear = "2015";
+        job2._endYear = "2020";
+
+
+        // ------------------------------ // 
+
+
+        Resume resume1 = new Resume();
+        resume1._name = "Json Bourne";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResumeDetails();
+
+
     }
 }
