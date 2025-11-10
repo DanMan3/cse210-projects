1a78d8d1d7b85e5b80b35574f096030527d148ba
DanMan3 <dhbennion@gmail.com>
2025-11-10 10:52:14 -0700

    Fractions activity completed

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        var firstNumber = new Fraction();
+        var secondNumber = new Fraction(4);
+        var thirdNumber = new Fraction(100, 13);
+        var fourthNumber = new Fraction(10, 5);
+
+        Console.WriteLine($"{firstNumber.GetFractionString()} --> {firstNumber.GetDecimalValue()}");
+        Console.WriteLine($"{secondNumber.GetFractionString()} --> {secondNumber.GetDecimalValue()}");
+        Console.WriteLine($"{thirdNumber.GetFractionString()} --> {thirdNumber.GetDecimalValue()}");
+        Console.WriteLine($"{fourthNumber.GetFractionString()} --> {fourthNumber.GetDecimalValue()}");
     }
+
 }
