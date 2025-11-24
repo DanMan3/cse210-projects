4fe198586e5060ebda6eb4cfa8db0edc15a775ee
DanMan3 <dhbennion@gmail.com>
2025-11-24 11:00:34 -0700

    Homework activity added.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("John Cena", "Science");
+        string assignmentSummary = assignment.GetSummary();
+        Console.WriteLine(assignmentSummary);
+
+
+        MathAssignment mathAssignment = new MathAssignment("Json Bourne", "Science", "6.7", "4-18");
+        var mathSummary = mathAssignment.GetSummary();
+        var homeworkList = mathAssignment.GetHomeworkList();
+        Console.WriteLine(mathSummary);
+        Console.WriteLine(homeworkList);
+
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Jane Barton", "Western Archaeology", "The Best of Western Archaeological Discoveries");
+        var writingSummary = writingAssignment.GetSummary();
+        var title = writingAssignment.GetWritingInformation();
+        Console.WriteLine(writingSummary);
+        Console.WriteLine(title);
+
+
     }
 }
