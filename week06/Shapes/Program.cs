05389a04c7aa2d35c319507b4a575e75d1afe74b
DanMan3 <dhbennion@gmail.com>
2025-11-25 10:14:35 -0700

    Polymorphism Shapes activity added.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        var shapes = new List<Shape>
+        {
+            new Square("Green", 2.3),
+            new Rectangle("Blue", 2.1, 5.2),
+            new Circle("Orange", 6.7)
+        };
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+
+        }
     }
 }
